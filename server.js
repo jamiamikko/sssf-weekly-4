f93de8c493e8ef250d4a888257a58e889fa142dd
jamiamikko <jamiamikko@gmail.com>
2018-08-08 13:32:39 +0300

    Delete comments

--- server.js
+++ server.js
@@ -91,19 +91,6 @@ db.on('error', (err) => {
   console.log(err);
 });
 
-// const imgDataSchema = new mongoose.Schema({
-//   time: {type: String, required: true},
-//   category: {type: String, required: true},
-//   title: {type: String, required: true},
-//   details: {type: String, required: true},
-//   coordinates: {type: Object, required: true},
-//   thumbnail: String,
-//   image: String,
-//   original: {type: String, required: true},
-// });
-
-// const ImgData = mongoose.model('ImgData', imgDataSchema);
-
 app.use('/images', images);
 
 app.post(
