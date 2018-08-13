5988bd82ab809ca601ed3a82be7c784149985507
jamiamikko <jamiamikko@gmail.com>
2018-08-13 19:22:51 +0300

    Finish template

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const indexContent = require('../views/index.json');
+const loginContent = require('../views/login.json');
 
 router.get('/', (req, res) => {
   if (req.user) {
@@ -15,7 +16,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-  res.render('login', {title: 'Login'});
+  res.render('login', {title: 'Login', content: loginContent});
 });
 
 module.exports = router;
